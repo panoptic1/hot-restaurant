6dd04f11fdca87946faa9aa3107ffc5273d10bdf
Hudso278 <Hudso278@umn.edu>
2018-06-02 17:39:23 -0500

    First Commit

---
+++ app/data/table-data.js
@@ -0,0 +1,10 @@
+var tableArray = [
+    {
+    customerName: 'Sean',
+    customerEmail: 'seanhudsonrsa@gmail.com',
+    customerID: 'qwerty1234',
+    phoneNumber: '651-304-0938'
+    }
+];
+
+module.exports = tableArray;
---
+++ app/data/waitlist-data.js
@@ -0,0 +1,10 @@
+var waitingArray = [
+    {
+    customerName: 'Hugh',
+    customerEmail: 'hughhudsonrsa@gmail.com',
+    customerID: 'asdfg5678',
+    phoneNumber: '651-304-0936'
+    }
+];
+
+module.exports = waitingArray;

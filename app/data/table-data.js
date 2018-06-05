2a1ed9c21042e3e5892ba37c7e7b84e3f507a039
Hudso278 <Hudso278@umn.edu>
2018-06-05 12:40:16 -0500

    Final Site

--- app/data/table-data.js
+++ app/data/table-data.js
@@ -4,6 +4,12 @@ var tableArray = [
     customerEmail: 'seanhudsonrsa@gmail.com',
     customerID: 'qwerty1234',
     phoneNumber: '651-304-0938'
+    },
+    {
+    customerName: 'Sean',
+    customerEmail: 'seanhudsonrsa@gmail.com',
+    customerID: 'qwerty1234',
+    phoneNumber: '651-304-0938'
     }
 ];
 
--- server.js
+++ server.js
@@ -8,20 +8,13 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
  
-app.get('/', function (req, res) {
-  res.send('Hello World');
-})
- 
 app.use(bodyParser.urlencoded({ extended: false }));
  
 // parse application/json
 app.use(bodyParser.json());
- 
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('you posted:\n');
-  res.end(JSON.stringify(req.body, null, 2));
-});
+
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
 
 app.listen(PORT, function(){
     console.log("App listening on PORT: " + PORT);
